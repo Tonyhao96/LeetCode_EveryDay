9889a42b4b7438a1cb953a42a57b9a27d4584d57
Tonyhao96 <673665244@qq.com>
2019-08-30 14:32:41 +0800

    38_Count and Say

--- 38_Count and Say.py
+++ 38_Count and Say.py
@@ -1,1 +1,17 @@
 #38_Count and Say
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        term='1'
+        for i in range(1,n):
+            next_term, count, number = '', 1, term[0]
+            
+            for j in range(1,len(term)):
+                if number==term[j]:
+                    count+=1
+                else:
+                    next_term+=str(count)+number
+                    number=term[j]
+                    count=1
+            next_term+=str(count)+number
+            term=next_term
+        return term
