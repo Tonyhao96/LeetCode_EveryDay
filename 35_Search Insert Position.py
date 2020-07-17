be36c71df614b56a5af4cb39e0d668fe5fa70e17
Tonyhao96 <673665244@qq.com>
2020-07-17 23:57:51 +0800

    35_Search Insert Position

--- 35_Search Insert Position.py
+++ 35_Search Insert Position.py
@@ -2,6 +2,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         for i in range(len(nums)):
-            if nums[i]>=target:
+            if nums[i] >= target:
                 return i
         return i+1
+
+# Using Bisection Method
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        if target > nums[right]:
+            return len(nums)
+        while left < right:
+            mid = ((right - left) >> 1) + left
+            if target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
