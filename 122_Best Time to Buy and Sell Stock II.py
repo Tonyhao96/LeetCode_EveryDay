f23cb08bc00c7e8747b8cff90f00f145b4d6199a
Tonyhao96 <673665244@qq.com>
2020-07-19 23:41:38 +0800

    122_Best Time to Buy and Sell Stock II

--- 122_Best Time to Buy and Sell Stock II.py
+++ 122_Best Time to Buy and Sell Stock II.py
@@ -23,5 +23,5 @@ class Solution:
         res = 0
         for i in range(len(prices) - 1):
             if prices[i+1] > prices[i]:
-                res += prices[i+1] -prices[i]
+                res += prices[i+1] - prices[i]
         return res
