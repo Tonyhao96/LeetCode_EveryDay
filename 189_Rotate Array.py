46f64d83253cccaa70f740b55ecc779eea53cef8
Tonyhao96 <673665244@qq.com>
2020-07-20 22:56:40 +0800

    189_Rotate Array

--- 189_Rotate Array.py
+++ 189_Rotate Array.py
@@ -12,3 +12,21 @@ class Solution:
             nums[1::] = nums[:-1]
             nums[0] = temp
 
+#Reverse 3 times
+#Time complexity: O(3n)  Space complexity: O(1)
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
